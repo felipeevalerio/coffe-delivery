9f595d04428e6634b9442de4cb22ac56d98720a4
Felipe Espindola Valerio <felipe.valerio@dtidigital.com.br>
2022-10-01 15:18:08 -0300

    implementando header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,26 @@
-import { HeaderContainer } from './styles';
+import { MapPin } from 'phosphor-react';
+import { Link } from 'react-router-dom';
+
+import { HeaderContainer, ActionsHeader, LocationInfo } from './styles';
+import logoImg from '../../assets/logo.svg';
+import { Cart } from '../Cart';
 
 export function Header() {
-  return <HeaderContainer></HeaderContainer>;
+  return (
+    <HeaderContainer>
+      <Link to="/">
+        <img
+          src={logoImg}
+          alt="Logotipo da Coffe Delivery, representado por um café com o logotipo da Rocketseat na parte mais interna e com um texto ao lado representando o nome do website 'Coffe Delivery'"
+        />
+      </Link>
+      <ActionsHeader>
+        <LocationInfo>
+          <MapPin weight="fill" size={22} />
+          <p>Porto Alegre, RS</p>
+        </LocationInfo>
+        <Cart />
+      </ActionsHeader>
+    </HeaderContainer>
+  );
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -3,5 +3,28 @@ import styled from 'styled-components';
 export const HeaderContainer = styled.header`
   display: flex;
   align-items: center;
+  justify-content: space-between;
   padding: 2rem;
+`;
+
+export const ActionsHeader = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.75rem;
+`;
+
+export const LocationInfo = styled.div`
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+  gap: 0.25rem;
+  border-radius: 8px;
+  padding: 0.5rem;
+
+  background: ${(props) => props.theme['purple-light']};
+  color: ${(props) => props.theme['purple-dark']};
+
+  p {
+    font-size: 0.875rem;
+  }
 `;
