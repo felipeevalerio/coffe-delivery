b1a71b99ef529cfecfa9241122ecd4b2ea1f0b4b
Felipe Espindola Valerio <felipe.valerio@dtidigital.com.br>
2022-10-01 14:39:29 -0300

    Adicionando Header no DefaultLayout

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  padding: 2rem;
+`;
--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
+import { Header } from '../../components/Header';
+import { LayoutContainer } from './styles';
 
 export function DefaultLayout() {
   return (
-    <>
-      <header></header>
+    <LayoutContainer>
+      <Header />
       <Outlet />
-    </>
+    </LayoutContainer>
   );
 }
