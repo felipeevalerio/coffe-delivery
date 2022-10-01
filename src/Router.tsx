e1c0f46a8a3b3e77eba454e50e9061f026b08d77
Felipe Espindola Valerio <felipe.valerio@dtidigital.com.br>
2022-10-01 15:16:46 -0300

    Importando Home

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import { DefaultLayout } from './layouts/DefaultLayout';
+import { Home } from './pages/Home';
 
 export function Router() {
   return (
