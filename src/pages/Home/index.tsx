801ff98d043e2679391c0bf85878769ef8f52e6c
Felipe Espindola Valerio <felipe.valerio@dtidigital.com.br>
2022-10-02 19:40:29 -0300

    Adicionando HomeContainer e criando coffeList

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,12 @@
+import { CoffeList } from './components/CoffeList';
 import { Intro } from './components/Intro';
+import { HomeContainer } from './styles';
 
 export function Home() {
   return (
-    <>
+    <HomeContainer>
       <Intro />
-    </>
+      <CoffeList />
+    </HomeContainer>
   );
 }
